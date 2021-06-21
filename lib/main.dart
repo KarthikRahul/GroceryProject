97e2bb6e89a32f09b303fe97cec9156577d3dea3
KarthikRahul <kannan10061972@gmail.com>
2021-06-21 21:23:47 +0530

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+void main(){
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
