a7391a58738ca56cf4329fe9b96602d0ad3b2af6
KarthikRahul <kannan10061972@gmail.com>
2021-06-21 21:24:25 +0530

    Firstcommit

---
+++ lib/login_screen.dart
@@ -0,0 +1,142 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:grocery_market/constants.dart';
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double height=MediaQuery.of(context).size.height;
+    double width=MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left:15.0,right: 15.0,top:100.0,bottom: 0),
+                child: Text("GROCERY STORE",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                )
+                  ,),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left:15.0,right: 300.0,top:100.0,bottom: 0),
+              child: Text("Login",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                  )
+                ,),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+             // padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+          Align(
+            alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15, bottom: 0),
+               child: FlatButton(
+                  onPressed: (){
+                    //TODO FORGOT PASSWORD SCREEN GOES HERE
+                  },
+                  child: Text(
+                    'Forgot Password',
+                    style: TextStyle(color: Colors.blue, fontSize: 15),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                   //AFTER PRESSING LOGIN SCREEN GOES HERE
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 0, bottom: 0),
+                child: FlatButton(
+                  onPressed: (){
+                    //AFTER PRESSING CREATE ACCOUNT
+                  },
+                  child: Text(
+                    'New User? Create Account',
+                    style: TextStyle(color: Colors.black, fontSize: 15),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.green, borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                    //AFTER PRESSING ADMIN BUTTON GOES HERE
+                  },
+                  child: Text(
+                    'Admin',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
